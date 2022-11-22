169d059d1eb5c7c8b038a70a6d80dc019b8f4a8b
eXplotion1337 <explotion1337@yandex.ru>
2022-11-22 16:25:50 +0700

    new version
    new v

--- cmd/shortener/shortener.go
+++ cmd/shortener/shortener.go
@@ -48,10 +48,66 @@ func BodyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+//var form = `<form name="form1" method="post" action="post.php">
+//Введите текст:<br />
+//<textarea name="text" cols="80" rows="10"></textarea>
+//<input name="" type="submit" value="Отправить"/>
+//</form>`
+//
+//func HelloWorld(w http.ResponseWriter, r *http.Request) {
+//	w.Write([]byte(form))
+//}
+var form = `<html>
+    <head>
+    <title></title>
+    </head>
+    <body>
+        <form action="/login" method="post">
+            <label>Логин</label><input type="text" name="login">
+            <label>Пароль<input type="password" name="password">
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>`
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	// проверяем, каким методом получили запрос
+	switch r.Method {
+	// если методом POST
+	case "POST":
+		login := r.FormValue("login")
+		password := r.FormValue("password")
+		// проверяем пароль вспомогательной функцией
+		if !Auth(login, password) {
+			w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+			// если пароль не верен, указываем код ошибки в заголовке
+			w.WriteHeader(401)
+			// пишем в тело ответа
+			fmt.Fprintln(w, r.Method)
+			return
+		}
+		// при успешной авторизации обрабатываем запрос
+		// например, передаём другому обработчику
+		//AuthorisedHandler(w, r)
+		// в остальных случаях предлагаем форму авторизации
+	default:
+		fmt.Fprint(w, form)
+	}
+}
+
+// Auth — вспомогательная функция авторизации
+// за пределами урока реализация может выглядеть так
+func Auth(l, p string) bool {
+	pass, ok := Logins[l]
+	return ok && p == pass
+}
+
+var Logins = make(map[string]string)
+
 // Старт сервера
 func main() {
 	//маршрутизация запросов обработчику
-	http.HandleFunc("/", BodyHandler)
+	http.HandleFunc("/", Login)
 
 	//запуск сервера с адресом localhost, порт 8080
 	http.ListenAndServe(":8080", nil)
