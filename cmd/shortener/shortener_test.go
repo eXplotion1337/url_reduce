d673944ecfadd47e9d52e35650354247ff765c62
eXplotion1337 <explotion1337@yandex.ru>
2022-11-23 15:16:01 +0700

    test http

---
+++ cmd/shortener/shortener_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"url_reduce/cmd/hendlers"
+	//"url_reduce/cmd/shortener"
+)
+
+func TestGetHandler(t *testing.T) {
+	type want struct {
+		code        int
+		response    string
+		contentType string
+	}
+	// создаём массив тестов: имя и желаемый результат
+	tests := []struct {
+		name string
+		want want
+	}{
+		// определяем все тесты
+		{
+			name: "positive test #1",
+			want: want{
+				code: 307,
+			},
+		},
+	}
+	for _, tt := range tests {
+		// запускаем каждый тест
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodGet, "/ser", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(hendlers.GetHandler)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+		})
+	}
+
+}
